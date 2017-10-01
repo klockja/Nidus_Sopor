73df45bf05dca46b54c1398db4dbef54cd5f75ee
James Klock <klockja@gmail.com>
2017-10-01 05:12:31 -0700

    Added A Protype Enemy
    Enemy is basically set up. I'm still working on detection.

---
+++ unity/Assets/Scripts/Enemy Scripts/AttackableEnemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackableEnemy : MonoBehaviour 
+{
+	private bool canBeHurt = true;
+
+	private float MaxHealth;
+	private float Health;
+
+
+	void Awake()
+	{
+//		Health = MaxHealth;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		Health = MaxHealth;	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnHit()
+	{
+		
+		if (Health <= 0) 
+		{
+			
+		}
+	}
+
+	public void SetMaxHealth(float newMaxHealth)
+	{
+		MaxHealth = newMaxHealth;
+	}
+
+	public float GetHealth()
+	{
+		return Health;
+	}
+}
---
+++ unity/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	public bool gamePaused = false;
+
+//	private Vector2 position; //The player's position in the world. Used for convienence.
+
+	private Rigidbody2D m_Body; //The player's rigidbody
+
+	[Header ("Player Statistics")]
+
+	public bool canMove = true;
+	private bool isAttacking = false;
+	[SerializeField]
+	private bool isSneaking = true;
+	[SerializeField]
+	private float currentSpeed = 0; //The current speed the player has while moving.
+	[SerializeField]
+	private float sneakSpeed = 1; //The default speed the player sneaks at.
+	[SerializeField]
+	private float runSpeed = 4; //The default speed the player runs at.
+
+	void Awake ()
+	{
+		m_Body = gameObject.GetComponentInChildren <Rigidbody2D> (); //Gets the Rigidbody of the character.
+	}
+
+	void Start () 
+	{
+		
+	}
+
+	void Update ()
+	{
+		if (gamePaused == false)
+		{
+				
+		}
+	}
+
+	void FixedUpdate () 
+	{
+		if (gamePaused == false)
+		{
+			PlayerMovement ();	
+		}
+	}
+
+	private void PlayerMovement() //Handles the player's movement.
+	{
+//		position = transform.position; //Sets the position variable as the players position.
+
+		if (canMove && isAttacking == false)
+		{
+			if (Input.GetKeyDown (KeyCode.LeftShift) || Input.GetKeyDown (KeyCode.RightShift))
+			{
+				if (isSneaking == true)
+				{
+					isSneaking = false;
+				} 
+				else
+				{
+					isSneaking = true;
+				}
+				
+			}
+
+			if (isSneaking == true)
+			{
+				currentSpeed = sneakSpeed;
+			} 
+			else
+			{
+				currentSpeed = runSpeed;
+			}
+
+			//up right
+			if (Input.GetAxisRaw ("Horizontal") > 0 && Input.GetAxisRaw ("Vertical") > 0)
+			{
+//				transform.Translate (new Vector2 (1, 1) * (currentSpeed - (currentSpeed / 4)) * Time.deltaTime);
+				m_Body.position += (new Vector2 (1, 1) * (currentSpeed - (currentSpeed / 4)) * Time.deltaTime);
+//				anim.SetBool ("isMoving", true);
+			}
+			//up left
+			else if (Input.GetAxisRaw ("Horizontal") < 0 && Input.GetAxisRaw ("Vertical") > 0)
+			{
+//				transform.Translate (new Vector2 (-1, 1) * (currentSpeed - (currentSpeed / 4)) * Time.deltaTime);
+				m_Body.position += (new Vector2 (-1, 1) * (currentSpeed - (currentSpeed / 4)) * Time.deltaTime);
+//				anim.SetBool ("isMoving", true);
+			}
+			//down right
+			else if (Input.GetAxisRaw ("Horizontal") > 0 && Input.GetAxisRaw ("Vertical") < 0)
+			{
+//				transform.Translate (new Vector2 (1, -1) * (currentSpeed - (currentSpeed / 4)) * Time.deltaTime);
+				m_Body.position += (new Vector2 (1, -1) * (currentSpeed - (currentSpeed / 4)) * Time.deltaTime);
+//				anim.SetBool ("isMoving", true);
+			}
+			//down left
+			else if (Input.GetAxisRaw ("Horizontal") < 0 && Input.GetAxisRaw ("Vertical") < 0)
+			{
+//				transform.Translate (new Vector2 (-1, -1) * (currentSpeed - (currentSpeed / 4)) * Time.deltaTime);
+				m_Body.position += (new Vector2 (-1, -1) * (currentSpeed - (currentSpeed / 4)) * Time.deltaTime);
+//				anim.SetBool ("isMoving", true);
+			}
+			//up
+			else if (Input.GetAxisRaw ("Vertical") > 0)
+			{
+//				transform.Translate (Vector2.up * currentSpeed * Time.deltaTime);
+				m_Body.position += (Vector2.up * currentSpeed * Time.deltaTime);
+
+//				anim.SetBool ("isMoving", true);
+//				anim.SetFloat ("input_y", 1);
+//				anim.SetFloat ("input_x", 0);
+//
+//				facingUp = true;
+//				facingDown = false;
+//				facingLeft = false;
+//				facingRight = false;
+			}
+			//down
+			else if (Input.GetAxisRaw ("Vertical") < 0)
+			{
+//				transform.Translate (Vector2.down * currentSpeed * Time.deltaTime);
+				m_Body.position += (Vector2.down * currentSpeed * Time.deltaTime);
+
+//				anim.SetBool ("isMoving", true);
+//				anim.SetFloat ("input_y", -1);
+//				anim.SetFloat ("input_x", 0);
+//
+//				facingUp = false;
+//				facingDown = true;
+//				facingLeft = false;
+//				facingRight = false;
+			}
+			//left
+			else if (Input.GetAxisRaw ("Horizontal") < 0)
+			{
+//				transform.Translate (Vector2.left * currentSpeed * Time.deltaTime);
+				m_Body.position += (Vector2.left * currentSpeed * Time.deltaTime);
+
+//				anim.SetBool ("isMoving", true);
+//				anim.SetFloat ("input_x", -1);
+//				anim.SetFloat ("input_y", 0);
+//
+//				facingUp = false;
+//				facingDown = false;
+//				facingLeft = true;
+//				facingRight = false;
+			}
+			//right
+			else if (Input.GetAxisRaw ("Horizontal") > 0)
+			{
+//				transform.Translate (Vector2.right * currentSpeed * Time.deltaTime);
+				m_Body.position += (Vector2.right * currentSpeed * Time.deltaTime);
+
+//				anim.SetBool ("isMoving", true);
+//				anim.SetFloat ("input_x", 1);
+//				anim.SetFloat ("input_y", 0);
+//
+//				facingUp = false;
+//				facingDown = false;
+//				facingLeft = false;
+//				facingRight = true;
+//			} else {
+//				anim.SetBool ("isMoving", false);
+//			}
+			}
+		}
+	}
+}
