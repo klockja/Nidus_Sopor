85a7017e48f7bc4b0655bd25be79b2a663ab2223
James Klock <klockja@gmail.com>
2017-10-02 19:45:14 -0700

    Made A Grid Making Script For Pathfinding

---
+++ unity/Assets/Scripts/Node.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour 
+{
+	public bool walkable;
+
+	public Vector2 worldPosition;
+
+	public Node(bool _walkable, Vector3 _worldPos)
+	{
+		walkable = _walkable;
+		worldPosition = _worldPos;
+	}
+}
