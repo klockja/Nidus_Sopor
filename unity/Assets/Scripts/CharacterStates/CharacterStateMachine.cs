1dbf6b6845ef93390b9788a29fdcb7ac2d0ef598
Matt <matt@matthewvroman.com>
2017-10-02 21:44:36 -0700

    Examples of state machines

---
+++ unity/Assets/Scripts/CharacterStates/CharacterStateMachine.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStateMachine 
+{
+    private PlayerController _playerController;
+    public PlayerController Controller
+    {
+        get
+        {
+            return _playerController;
+        }
+    }
+
+    private CharacterState _currentState;
+    public CharacterState CurrentState
+    {
+        get
+        {
+            return _currentState;
+        }
+        set
+        {
+            if(_currentState != null)
+            {
+                _currentState.OnExit();
+            }
+            _currentState = value;
+            _currentState.OnEnter();
+        }
+    }
+
+	public CharacterStateMachine(PlayerController playerController)
+    {
+        _playerController = playerController;
+    }
+
+    public void Update()
+    {
+        if(_currentState != null)
+        {
+            _currentState.Update();
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        if(_currentState != null)
+        {
+            _currentState.FixedUpdate();
+        }
+    }
+
+
+	
+}
--- unity/Assets/Scripts/Player Scripts/PlayerController.cs
+++ unity/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -9,23 +9,62 @@ public class PlayerController : MonoBehaviour
 //	private Vector2 position; //The player's position in the world. Used for convienence.
 
 	private Rigidbody2D m_Body; //The player's rigidbody
+	public Rigidbody2D Body
+	{
+		get
+		{
+			return m_Body;
+		}
+	}
 
 	[Header ("Player Statistics")]
 
 	public bool canMove = true;
-	private bool isAttacking = false;
-	[SerializeField]
-	private bool isSneaking = true;
-	[SerializeField]
-	private float currentSpeed = 0; //The current speed the player has while moving.
+
 	[SerializeField]
 	private float sneakSpeed = 1; //The default speed the player sneaks at.
+	public float SneakSpeed
+	{
+		get
+		{
+			return sneakSpeed;
+		}
+	}
+
 	[SerializeField]
 	private float runSpeed = 4; //The default speed the player runs at.
+	public float RunSpeed
+	{
+		get
+		{
+			return runSpeed;
+		}
+	}
+
+	private float m_speedDecay = 1.0f;
+    public float SpeedDecay
+    {
+        get
+        {
+            return m_speedDecay;
+        }
+    }
+
+	private CharacterStateMachine m_stateMachine;
+	public CharacterStateMachine StateMachine
+	{
+		get
+		{
+			return m_stateMachine;
+		}
+	}
 
 	void Awake ()
 	{
 		m_Body = gameObject.GetComponentInChildren <Rigidbody2D> (); //Gets the Rigidbody of the character.
+
+		m_stateMachine = new CharacterStateMachine(this);
+		m_stateMachine.CurrentState = new MovementState(m_stateMachine);
 	}
 
 	void Start () 
@@ -37,7 +76,40 @@ public class PlayerController : MonoBehaviour
 	{
 		if (gamePaused == false)
 		{
-				
+			m_stateMachine.Update();
+
+			float horizontal = Input.GetAxis ("Horizontal");
+			float vertical = Input.GetAxis ("Vertical");
+
+			Vector2 movementDirection = new Vector2(horizontal, vertical);
+			movementDirection.Normalize();
+			m_stateMachine.CurrentState.DesireMovement(movementDirection);
+
+			if(Input.GetKeyDown(KeyCode.LeftShift))
+			{
+				m_stateMachine.CurrentState.DesireSneakToggle();
+			}	
+
+			if(Input.GetKeyDown(KeyCode.Space))
+			{
+				m_stateMachine.CurrentState.DesireShoot();
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if(collider.tag == "TallGrass")
+		{
+			m_speedDecay = 0.5f;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D collider)
+	{
+		if(collider.tag == "TallGrass")
+		{
+			m_speedDecay = 1.0f;
 		}
 	}
 
@@ -45,129 +117,7 @@ public class PlayerController : MonoBehaviour
 	{
 		if (gamePaused == false)
 		{
-			PlayerMovement ();	
-		}
-	}
-
-	private void PlayerMovement() //Handles the player's movement.
-	{
-//		position = transform.position; //Sets the position variable as the players position.
-
-		if (canMove && isAttacking == false)
-		{
-			if (Input.GetKeyDown (KeyCode.LeftShift) || Input.GetKeyDown (KeyCode.RightShift))
-			{
-				if (isSneaking == true)
-				{
-					isSneaking = false;
-				} 
-				else
-				{
-					isSneaking = true;
-				}
-				
-			}
-
-			if (isSneaking == true)
-			{
-				currentSpeed = sneakSpeed;
-			} 
-			else
-			{
-				currentSpeed = runSpeed;
-			}
-
-			//up right
-			if (Input.GetAxisRaw ("Horizontal") > 0 && Input.GetAxisRaw ("Vertical") > 0)
-			{
-//				transform.Translate (new Vector2 (1, 1) * (currentSpeed - (currentSpeed / 4)) * Time.deltaTime);
-				m_Body.position += (new Vector2 (1, 1) * (currentSpeed - (currentSpeed / 4)) * Time.deltaTime);
-//				anim.SetBool ("isMoving", true);
-			}
-			//up left
-			else if (Input.GetAxisRaw ("Horizontal") < 0 && Input.GetAxisRaw ("Vertical") > 0)
-			{
-//				transform.Translate (new Vector2 (-1, 1) * (currentSpeed - (currentSpeed / 4)) * Time.deltaTime);
-				m_Body.position += (new Vector2 (-1, 1) * (currentSpeed - (currentSpeed / 4)) * Time.deltaTime);
-//				anim.SetBool ("isMoving", true);
-			}
-			//down right
-			else if (Input.GetAxisRaw ("Horizontal") > 0 && Input.GetAxisRaw ("Vertical") < 0)
-			{
-//				transform.Translate (new Vector2 (1, -1) * (currentSpeed - (currentSpeed / 4)) * Time.deltaTime);
-				m_Body.position += (new Vector2 (1, -1) * (currentSpeed - (currentSpeed / 4)) * Time.deltaTime);
-//				anim.SetBool ("isMoving", true);
-			}
-			//down left
-			else if (Input.GetAxisRaw ("Horizontal") < 0 && Input.GetAxisRaw ("Vertical") < 0)
-			{
-//				transform.Translate (new Vector2 (-1, -1) * (currentSpeed - (currentSpeed / 4)) * Time.deltaTime);
-				m_Body.position += (new Vector2 (-1, -1) * (currentSpeed - (currentSpeed / 4)) * Time.deltaTime);
-//				anim.SetBool ("isMoving", true);
-			}
-			//up
-			else if (Input.GetAxisRaw ("Vertical") > 0)
-			{
-//				transform.Translate (Vector2.up * currentSpeed * Time.deltaTime);
-				m_Body.position += (Vector2.up * currentSpeed * Time.deltaTime);
-
-//				anim.SetBool ("isMoving", true);
-//				anim.SetFloat ("input_y", 1);
-//				anim.SetFloat ("input_x", 0);
-//
-//				facingUp = true;
-//				facingDown = false;
-//				facingLeft = false;
-//				facingRight = false;
-			}
-			//down
-			else if (Input.GetAxisRaw ("Vertical") < 0)
-			{
-//				transform.Translate (Vector2.down * currentSpeed * Time.deltaTime);
-				m_Body.position += (Vector2.down * currentSpeed * Time.deltaTime);
-
-//				anim.SetBool ("isMoving", true);
-//				anim.SetFloat ("input_y", -1);
-//				anim.SetFloat ("input_x", 0);
-//
-//				facingUp = false;
-//				facingDown = true;
-//				facingLeft = false;
-//				facingRight = false;
-			}
-			//left
-			else if (Input.GetAxisRaw ("Horizontal") < 0)
-			{
-//				transform.Translate (Vector2.left * currentSpeed * Time.deltaTime);
-				m_Body.position += (Vector2.left * currentSpeed * Time.deltaTime);
-
-//				anim.SetBool ("isMoving", true);
-//				anim.SetFloat ("input_x", -1);
-//				anim.SetFloat ("input_y", 0);
-//
-//				facingUp = false;
-//				facingDown = false;
-//				facingLeft = true;
-//				facingRight = false;
-			}
-			//right
-			else if (Input.GetAxisRaw ("Horizontal") > 0)
-			{
-//				transform.Translate (Vector2.right * currentSpeed * Time.deltaTime);
-				m_Body.position += (Vector2.right * currentSpeed * Time.deltaTime);
-
-//				anim.SetBool ("isMoving", true);
-//				anim.SetFloat ("input_x", 1);
-//				anim.SetFloat ("input_y", 0);
-//
-//				facingUp = false;
-//				facingDown = false;
-//				facingLeft = false;
-//				facingRight = true;
-//			} else {
-//				anim.SetBool ("isMoving", false);
-//			}
-			}
+			m_stateMachine.FixedUpdate();
 		}
 	}
 }
