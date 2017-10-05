02dbf98d0221e07f93a078e8c386b2823fb518a9
chuang4305 <chuang@mail.bradley.edu>
2017-10-05 11:01:20 +0800

    Add in State Machine files
    just adding files for state machines to look at

---
+++ unity/Assets/Scripts/State Machine/MovementState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementState : CharacterState { 
+
+	public MovementState(CharacterStateMachine machine):base(machine)
+	{
+	}
+
+	override public void DesireMovement(Vector2 movement)
+	{
+		float speed = m_machine.Controller.RunSpeed * m_machine.Controller.SpeedDecay;
+
+		m_machine.Controller.Body.velocity = (movement * speed * Time.deltaTime);
+	}
+
+	override public void DesireSneakToggle()
+	{
+		m_machine.CurrentState = new SneakState(m_machine);
+	}
+
+	override public void DesireShoot()
+	{
+		m_machine.CurrentState = new AttackState(m_machine, this);
+	}
+}
---
+++ unity/Assets/Scripts/State Machine/PlayerController.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	public bool gamePaused = false;
+
+//	private Vector2 position; //The player's position in the world. Used for convienence.
+
+	private Rigidbody2D m_Body; //The player's rigidbody
+	public Rigidbody2D Body
+	{
+		get
+		{
+			return m_Body;
+		}
+	}
+
+	[Header ("Player Statistics")]
+
+	public bool canMove = true;
+
+	[SerializeField]
+	private float sneakSpeed = 1; //The default speed the player sneaks at.
+	public float SneakSpeed
+	{
+		get
+		{
+			return sneakSpeed;
+		}
+	}
+
+	[SerializeField]
+	private float runSpeed = 4; //The default speed the player runs at.
+	public float RunSpeed
+	{
+		get
+		{
+			return runSpeed;
+		}
+	}
+
+	private float m_speedDecay = 1.0f;
+    public float SpeedDecay
+    {
+        get
+        {
+            return m_speedDecay;
+        }
+    }
+
+	private CharacterStateMachine m_stateMachine;
+	public CharacterStateMachine StateMachine
+	{
+		get
+		{
+			return m_stateMachine;
+		}
+	}
+
+	void Awake ()
+	{
+		m_Body = gameObject.GetComponentInChildren <Rigidbody2D> (); //Gets the Rigidbody of the character.
+
+		m_stateMachine = new CharacterStateMachine(this);
+		m_stateMachine.CurrentState = new MovementState(m_stateMachine);
+	}
+
+	void Start () 
+	{
+		
+	}
+
+	void Update ()
+	{
+		if (gamePaused == false)
+		{
+			m_stateMachine.Update();
+
+			float horizontal = Input.GetAxis ("Horizontal");
+			float vertical = Input.GetAxis ("Vertical");
+
+			Vector2 movementDirection = new Vector2(horizontal, vertical);
+			movementDirection.Normalize();
+			m_stateMachine.CurrentState.DesireMovement(movementDirection);
+
+			if(Input.GetKeyDown(KeyCode.LeftShift))
+			{
+				m_stateMachine.CurrentState.DesireSneakToggle();
+			}	
+
+			if(Input.GetKeyDown(KeyCode.Space))
+			{
+				m_stateMachine.CurrentState.DesireShoot();
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if(collider.tag == "TallGrass")
+		{
+			m_speedDecay = 0.5f;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D collider)
+	{
+		if(collider.tag == "TallGrass")
+		{
+			m_speedDecay = 1.0f;
+		}
+	}
+
+	void FixedUpdate () 
+	{
+		if (gamePaused == false)
+		{
+			m_stateMachine.FixedUpdate();
+		}
+	}
+}
