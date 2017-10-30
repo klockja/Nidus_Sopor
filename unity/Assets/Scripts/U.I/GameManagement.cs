ba14f1cabdcffed578a30d41880bdabdf6b1897d
chuang4305 <chuang@mail.bradley.edu>
2017-10-31 05:59:49 +0800

    Fix Script
    FIx Script

--- unity/Assets/Scripts/U.I/GameManagement.cs
+++ unity/Assets/Scripts/U.I/GameManagement.cs
@@ -7,13 +7,13 @@ public class GameManagement : MonoBehaviour {
 
 
 	public bool isPaused;
-	//public bool goingBackwards;
+	public bool goingBackwards;
 	public bool gameover;
 	public bool nextScene;
 	public float bulletCount;
 	public float rockCount;
-	public float defaultBulletCount;
-	public float defaultRockCount;
+	private float defaultBulletCount;
+	private float defaultRockCount;
 	public AudioSource musicPlayer;
 	public AudioClip backgroundMusic;
 
@@ -47,6 +47,12 @@ public class GameManagement : MonoBehaviour {
 			bulletCount = defaultBulletCount;
 			rockCount = defaultRockCount;
 
+		}
+
+		if (goingBackwards == true && SceneManager.GetActiveScene ().name == "Cave") 
+		{
+			GameObject.Find ("ForwardPortal").SetActive (false);
+			GameObject.Find ("BackwardPortal").SetActive (true);
 		}
 	}
 }
