9414d0ea6c11b83104a64d41227f53baed7010cb
James Klock <klockja@gmail.com>
2017-11-06 21:24:30 -0800

    Deleted Dadako Fonts and Added Teleport Script

---
+++ unity/Assets/Scripts/TeleportTriggerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportTriggerScript : MonoBehaviour {
+
+	[SerializeField]
+	private Vector2 destination;
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.name == "Player")
+		{
+			col.transform.position = destination;
+		}
+
+	}
+}
