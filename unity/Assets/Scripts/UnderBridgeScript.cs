c4d2dfc4971d53c96efc2fd03a46a7e42c672b96
James Klock <klockja@gmail.com>
2017-11-12 00:22:42 -0800

    Added A Sprite Diffuse Shader To Apply to All Sprites, Allows for Lighting

---
+++ unity/Assets/Scripts/UnderBridgeScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderBridgeScript : MonoBehaviour {
+
+	public GameObject[] OverColliders;
+	public GameObject[] UnderColliders;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.tag == "Player" || col.tag == "Enemy")
+		{
+			GetComponentInChildren <SpriteRenderer> ().sortingOrder = 0;
+			for (int i = 0; i < OverColliders.Length; i++)
+			{
+				OverColliders [i].SetActive (false);
+			}
+			for (int i = 0; i < UnderColliders.Length; i++)
+			{
+				UnderColliders [i].SetActive (true);
+			}
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D col)
+	{
+		if (col.tag == "Player" || col.tag == "Enemy")
+		{
+			GetComponentInChildren <SpriteRenderer> ().sortingOrder = 2;
+			for (int i = 0; i < OverColliders.Length; i++)
+			{
+				OverColliders [i].SetActive (true);
+			}
+			for (int i = 0; i < UnderColliders.Length; i++)
+			{
+				UnderColliders [i].SetActive (false);
+			}
+		}
+	}
+}
