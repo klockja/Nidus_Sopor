8a153b45dfc78215000495222ab8d4939632b3fc
James Klock <klockja@gmail.com>
2017-11-14 23:36:36 -0800

    Fixed Sound Detection

--- unity/Assets/Scripts/SmellTriggerScript.cs
+++ unity/Assets/Scripts/SmellTriggerScript.cs
@@ -15,4 +15,12 @@ public class SmellTriggerScript : MonoBehaviour
 			Destroy (gameObject);
 		}
 	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag == "Flowers")
+		{
+			Destroy (gameObject);
+		}
+	}
 }
