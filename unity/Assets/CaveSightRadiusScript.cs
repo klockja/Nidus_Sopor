c0ad3b167dc4012cb59f876ce18c9182c00f60c8
James Klock <klockja@gmail.com>
2017-11-25 19:59:38 -0800

    Added Cave Sight Radius

---
+++ unity/Assets/CaveSightRadiusScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveSightRadiusScript : MonoBehaviour {
+
+	private GameObject Player;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		Player = GameManagement.Instance.player;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate ((Player.transform.position - transform.position) * Time.deltaTime);
+	}
+}
