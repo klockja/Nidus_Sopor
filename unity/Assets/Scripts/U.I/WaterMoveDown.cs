b3feef3e96db88b6a078ae439e857ecf7c942a1f
chuang4305 <chuang@mail.bradley.edu>
2017-12-03 17:21:57 +0800

    stuff
    stuff

---
+++ unity/Assets/Scripts/U.I/WaterMoveDown.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMoveDown : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = Vector2.MoveTowards (transform.position, new Vector2 (0, -24), 1 * Time.deltaTime);
+
+		if (transform.position.y < -12) {
+			transform.position = new Vector2 (0,35);
+
+		}
+	}
+}
+
+
