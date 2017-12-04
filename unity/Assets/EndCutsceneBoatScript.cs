d18e0454da1258c4ca89e30c9b3294289757ea8b
James Klock <klockja@gmail.com>
2017-12-03 16:58:55 -0800

    Added Ending

---
+++ unity/Assets/EndCutsceneBoatScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndCutsceneBoatScript : MonoBehaviour {
+
+	public float timerTillDisappear;
+
+	Animator anim;
+
+	public Dialogue DialogueScript;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent <Animator> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (DialogueScript.DialogueEnded)
+		{
+			timerTillDisappear -= Time.deltaTime;
+
+			if (timerTillDisappear <= 0)
+			{
+				anim.SetBool ("SailAway", true);
+			}
+		}
+	}
+}
