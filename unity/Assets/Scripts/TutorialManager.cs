87248b398297fe6e5c56124673c52296a2dfb716
chuang4305 <chuang@mail.bradley.edu>
2018-02-10 04:09:47 +0800

    beach stuff
    beach stuff

---
+++ unity/Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager: GenericSingletonClass<TutorialManager> {
+
+	public GameObject point1;
+	public GameObject point2;
+	public float pointNumber;
+
+	// Use this for initialization
+	void Start () {
+
+		point1.SetActive (true);
+		point2.SetActive (false);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (point1.activeSelf == true) {
+			point2.SetActive (false);
+		} else if (point2.activeSelf == true) {
+			point1.SetActive (false);
+		}
+	}
+		
+}
---
+++ unity/Assets/Scripts/TutorialPointScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPointScript : MonoBehaviour {
+
+	private float num;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Player")  
+		{
+			num = TutorialManager.Instance.
+
+
+		}
+	} 
+}
